0a51effade9ec8470abfd0ba4c43d44510222ff2
Nikita <neymarenko123@gmail.com>
2020-10-27 22:49:19 +0200

    the last

--- src/pages/secretPage/secretPage.jsx
+++ src/pages/secretPage/secretPage.jsx
@@ -11,11 +11,14 @@ import Chelick from "./chelick";
 const SecretPage = ({isLogined}) => {
     const history = useHistory();
     useEffect(() => {
-        if (!isLogined) history.push("/#")
+        if (!isLogined) {
+            history.push("/#");
+            alert("nonono");
+        }
     }, [isLogined]);
     return (
         <div className="secret-page w-100">
-            <header className="secret-page-header"><img src={logo} width="40px" alt="chelik"/>внутрішній документ <small>№0000032478123</small></header>
+            <header className="secret-page-header"><img src={logo} width="40px" alt="chelik"/><span className="min-width-520">внутрішній документ</span> <small>№0000032478123</small></header>
             <div className="hr-line"/>
             <main className="secret-page-content">
                 <Chelick img={chelick1}/>
