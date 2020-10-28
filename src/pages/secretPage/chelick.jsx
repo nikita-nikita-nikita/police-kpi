38d5ea8a6a53ab8619f2155363987ccc8574032c
Nikita <neymarenko123@gmail.com>
2020-10-28 22:53:02 +0200

    add data

--- src/pages/secretPage/chelick.jsx
+++ src/pages/secretPage/chelick.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Chelick = ({img}) => {
+const Chelick = ({img, text, data:{phone, name}}) => {
     return (
         <div className="chelick">
             <h2>Протокол допиту підозрюванного</h2>
@@ -26,7 +26,7 @@ const Chelick = ({img}) => {
                 </div>
             </div>
             <div className="bottom-content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi delectus ducimus excepturi fuga illum nesciunt pariatur quaerat quidem. Aspernatur dolorem dolores, eveniet expedita inventore ipsa, iure, laudantium modi quae quos tenetur unde veniam voluptatibus. Commodi, dolor dolores inventore optio placeat porro quibusdam rem totam veniam! A adipisci alias aspernatur consectetur consequuntur cum dignissimos doloremque eaque, eius eum expedita explicabo fugit illum itaque iusto laborum magnam maiores modi molestiae natus neque optio, perferendis perspiciatis quas quisquam repudiandae saepe suscipit tenetur vel, voluptates. Aperiam est illo laborum mollitia possimus quo voluptatem? Accusantium assumenda atque blanditiis dolore doloribus dolorum ducimus est recusandae totam!
+                {text}
             </div>
         </div>
     )
